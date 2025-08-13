234675845540d19e0009559ce8c91144be01ec58
a-bakos <bakosattila@fastem.com>
2025-08-13 20:08:04 +0100

    i18n basic prototype

--- gui/js/app.js
+++ gui/js/app.js
@@ -1,1 +1,15 @@
+import Lang from './lang.js'
+
+const LANG_EN = 'en'
+const LANG_HU = 'hu'
+const i18n = new Lang(LANG_EN);
+
+(async () => {
+    await i18n.init()
+    console.log(i18n.t('greeting', { name: 'John' }))
+
+    i18n.setLang(LANG_HU)
+    console.log(i18n.t('greeting', { name: 'John' }))
+})()
+
 // import { BookManager } from './js/book.js'
---
+++ gui/js/lang.js
@@ -0,0 +1,38 @@
+export default class Lang {
+    static LANG_FILE = '../lang.json'
+    static LANG_EN = 'en'
+    static DICT_SPLIT = '.'
+
+    constructor(defaultLang = Lang.LANG_EN) {
+        this.dictionary = {}
+        this.currentLang = defaultLang
+    }
+
+    async init(langCode = this.currentLang) {
+        const res = await fetch(Lang.LANG_FILE)
+        this.dictionary = await res.json()
+        this.currentLang = langCode
+    }
+
+    setLang(langCode) {
+        this.currentLang = langCode
+    }
+
+    // Translation function
+    t(key, vars = {}) {
+        const parts = key.split(Lang.DICT_SPLIT)
+        let value = this.dictionary
+        for (let p of parts) {
+            value = value?.[p]
+            if (!value) return key // fallback
+        }
+        let text = value[this.currentLang] || key
+
+        // Replace {{placeholders}}
+        for (const [k, v] of Object.entries(vars)) {
+            text = text.replace(new RegExp(`{{${k}}}`, 'g'), v)
+        }
+
+        return text
+    }
+}
