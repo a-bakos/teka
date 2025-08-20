2c7feda6abd74d4af6af8957b925e492fed85665
a-bakos <bakosattila@fastem.com>
2025-08-20 21:35:18 +0100

    Sketching JS functions

---
+++ gui/frontend/src/ScreenBookList.js
@@ -0,0 +1,13 @@
+"use strict"
+
+export default class ScreenBookList {
+    constructor() {
+        return this.render();
+    }
+
+    render() {
+        const div = document.createElement("div");
+        div.innerHTML = '<h1 class="bg-red-500">Books</h1>';
+        return div;
+    }
+}
